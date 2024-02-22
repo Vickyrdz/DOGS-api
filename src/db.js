ca647af7bdd7034ada48d96ac93e0cee17164a43
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-02-22 00:05:04 -0300

    cambio config db

--- src/db.js
+++ src/db.js
@@ -6,9 +6,12 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
+const pg = require("pg");
+
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectModule: pg
 });
 const basename = path.basename(__filename);
 
