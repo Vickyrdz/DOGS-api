bbdd2a5e7f331055a9dada30be00e4b59a9e0d21
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-02-29 02:34:20 -0300

    actualizo configuracion db y temperamentos

--- src/controllers/getTemperaments.js
+++ src/controllers/getTemperaments.js
@@ -4,6 +4,9 @@ const { API_KEY } = process.env;
 
 const getAllTemps = async (req, res) => {
     try {
+        // busco los de la BD
+        const foundTemps = await Temperament.findAll(); 
+
         // todos los perros de la API
         const { data } = await axios("https://api.thedogapi.com/v1/breeds", {
             params: { api_key: API_KEY }
@@ -12,20 +15,19 @@ const getAllTemps = async (req, res) => {
         // de cada uno agarro el temperamento y lo separo por ','
         // con esa lista de temperamentos separados veo si ya lo guardé en mi BD
         // si no, lo creo para tenerlo guardado para próximas búsquedas
-        data.forEach(dog => {
-            if (dog.temperament) {
-                let temps = dog.temperament.split(","); 
-                temps.forEach(dogTemps => {
-                    //busca en el modelo el temperamento y sino lo crea y agrega
-                    Temperament.findOrCreate({
-                        where:  {name: dogTemps}
-                    })
-                });
-            }
-        });
-
-        // busco los de la BD
-        const foundTemps = await Temperament.findAll(); 
+        if (!foundTemps?.length) {
+            data.forEach(dog => {
+                if (dog.temperament) {
+                    let temps = dog.temperament.split(","); 
+                    temps.forEach(dogTemps => {
+                        //busca en el modelo el temperamento y sino lo crea y agrega
+                        Temperament.findOrCreate({
+                            where:  {name: dogTemps}
+                        })
+                    });
+                }
+            });
+        }
         
         const sortedFoundTemps = foundTemps.sort((a,b) => a.id - b.id) //se ordenan de forma ascendente 
         return res.status(200).json(sortedFoundTemps);
--- src/db.js
+++ src/db.js
@@ -6,13 +6,23 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-const pg = require("pg");
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectModule: pg
+const sequelize = new Sequelize({
+  database: DB_NAME,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: 5432,
+  dialect: 'postgres',
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
